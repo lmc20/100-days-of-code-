c14143b4880deef36ac7c2ddf0f81d8e811db0bc
Love Much Chandra <38309830+lmc20@users.noreply.github.com>
2018-12-18 22:13:59 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the values of a and b");
+
+           
+            
+
+            switch (num)
+            {
+                case 1:  
+                    a = Convert.ToInt32(Console.ReadLine());
+                   
+
+                    b = Convert.ToInt32(Console.ReadLine());
+
+
+                    c = a + b;
+
+                    Console.WriteLine(c);
+                    Console.ReadKey();
+
+                    break;
+
+                case 2:
+                    a = Convert.ToInt32(Console.ReadLine());
+
+
+                    b = Convert.ToInt32(Console.ReadLine());
+
+
+                    c = a - b;
+
+                    Console.WriteLine(c);
+                    Console.ReadKey();
+
+                    break;
+
+                case 3:
+                    a = Convert.ToInt32(Console.ReadLine());
+
+
+                    b = Convert.ToInt32(Console.ReadLine());
+
+
+                    c = a * b;
+
+                    Console.WriteLine(c);
+                    Console.ReadKey();
+
+                    break;
+
+                case 4:
+                    a = Convert.ToInt32(Console.ReadLine());
+
+
+                    b = Convert.ToInt32(Console.ReadLine());
+
+
+                    c = a / b;
+
+                    Console.WriteLine(c);
+                    Console.ReadKey();
+
+                    break;
+            }
+        }
+    }
+}
