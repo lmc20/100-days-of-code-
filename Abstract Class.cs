9883b4ffa09d11522258d861376f90e3a30a5a4b
Love Much Chandra <38309830+lmc20@users.noreply.github.com>
2018-12-24 19:19:10 +0530

    Add files via upload

---
+++ Abstract Class.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+
+    public abstract class Customer
+    {
+        public abstract void Print();
+
+    }
+
+    public class Product : Customer
+    {
+        public string name;
+        public int price; 
+
+        public override void Print()
+        {
+            Console.WriteLine("Name:" +name); 
+            Console.WriteLine("Price:" +price) ;
+            Console.ReadKey();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Pizza = new Product();
+            Pizza.name = "CheeseBurst";
+            Pizza.price = 200;
+            Pizza.Print();
+
+   
+        }
+    }
+}
---
+++ Method Hiding.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    public class Employee
+    {
+
+        public string FirstName;
+        public string LastName;
+
+        public void Print()
+        {
+            Console.WriteLine("Employee's Name: " + FirstName + " " + LastName);
+            Console.ReadKey();
+        }
+
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public new void Print()                                                              //Method Hides the Base Method
+        {
+            Console.WriteLine("Employee's Name: " + FirstName + " " + LastName+ "-" );
+            Console.ReadKey();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee Employee = new FullTimeEmployee();
+            Employee.FirstName = "Love";
+            Employee.LastName = "Chandra";
+            Employee.Print();
+
+            PartTimeEmployee PEmployee = new PartTimeEmployee();
+            PEmployee.FirstName = "Bret";
+            PEmployee.LastName = "Hart";
+            PEmployee.Print();                                                       // Inherited Class Method
+
+
+            ( (Employee)PEmployee).Print();                                          //Calling the Base Class Method
+
+
+
+        }
+    }
+}
