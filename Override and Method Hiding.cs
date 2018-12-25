8a422b2284e4eca6416e5c754eb502b15a5e9be0
Love Much Chandra <38309830+lmc20@users.noreply.github.com>
2018-12-25 21:02:17 +0530

    Add files via upload

---
+++ Override and Method Hiding.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass b = new DerivedClass1();
+            b.Print();
+        }
+    }
+
+    class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("This is a base class");
+            Console.ReadKey();
+
+        }
+    }
+
+    class DerivedClass  : BaseClass
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is a derived class");
+            Console.ReadKey();
+        }
+    }
+
+   class DerivedClass1 : BaseClass
+    {
+        public new void Print()
+        {
+            Console.WriteLine("This is a derived1 class");
+            Console.ReadKey();
+        }
+    }
+
+
+}
+
---
+++ Polymorphism.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] Employees = new Employee[3];                        // Employees[]  is a base class referance variable  
+
+            Employees[0] = new Employee();                                 //  Employees is a base class object
+            Employees[1] = new FullTimeEmployee();                         //  FullTimeEmployees  is a derived class object
+            Employees[2] = new PartTimeEmployee();                         //  PartTimeEmployee  is a derived class object 
+
+            foreach (Employee e in Employees)
+            {
+                e.Print();
+                Console.WriteLine();
+                Console.ReadKey();                
+            }
+        }
+    }
+
+    public class Employee
+    {
+       public string FirstName = "FN";
+       public string LastName = "LN";
+        
+        public virtual void Print()
+        {
+            Console.WriteLine("First Name: " +FirstName+ "Last Name: " +LastName);
+
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+
+        public override void Print()
+        {
+            Console.WriteLine("First Name: " + FirstName + "Last Name: " + LastName+ "-");
+
+        }
+    }
+
+    public class PartTimeEmployee :Employee
+    {
+        
+        public override void Print()
+        {
+            Console.WriteLine("First Name: " + FirstName + "Last Name: " + LastName+ "--");
+
+        }
+    }
+
+
+}
