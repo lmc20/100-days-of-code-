7aa550edc7984be901ef047d121eb7d9e0ea7385
Love Much Chandra <38309830+lmc20@users.noreply.github.com>
2018-12-30 00:14:43 +0530

    Add files via upload

---
+++ Obsolete Attribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp31
+{
+    class Calculator
+    {   
+        [Obsolete ("Use Add List Method")]
+        public static void Add(int a, int b)
+        {
+            int sum = a + b; 
+            Console.WriteLine(sum);
+        }
+
+        public void Add(List<int> Numbers)
+        {
+            int Sum = 0;
+
+            foreach(int number in Numbers)
+            {
+                Sum = Sum + number;
+                
+            }
+
+            Console.WriteLine(Sum);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calculator.Add(3,3);
+            Console.ReadKey();
+        }
+    }
+
+
+}
+
